a34d7d54f1aaf835f3de4ae83536220aaf501d06
alex-daley <adaley.gamedev@gmail.com>
2021-11-04 22:15:19 +0000

    Initial game board layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import './app.css'
+
+function seq(end: number) {
+  const array = []
+  for (let i = 1; i <= end; i++)
+    array.push(i)
+  return array
+}
+
 function App() {
   return (
-    <div>
-      combocity
+    <div className="game-container">
+      <div className="board">
+        {seq(25).map(index => (
+          <div className="cell-container" key={`cell-${index}`}>
+            <div className="cell">{index}</div>
+          </div>
+        ))}
+      </div>
     </div>
-  );
+  ) 
 }
 
 export default App
