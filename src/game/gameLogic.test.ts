e931bc742d309a6150afa104a6968594b3cdab0c
alex-daley <adaley.gamedev@gmail.com>
2021-11-15 19:41:42 +0000

    Better tests

--- src/game/gameLogic.test.ts
+++ src/game/gameLogic.test.ts
@@ -1,48 +1,140 @@
-import * as gameLogic from './gameLogic'
+import * as game from './gameLogic'
 
-const numCols = gameLogic.numCols
-const numRows = gameLogic.numRows
+function createBoard(...args: [index: number, zone: game.Zone, value: number][]) {
+  const board = game.createBoard()
+  args.forEach(([index, zone, value]) => {
+    board[index] = new game.Square(zone, value)
+  })
 
-type Square = gameLogic.Square
-type MoveFunction = (board: Square[]) => Square[]
-
-function expectMove(move: MoveFunction, startAt: number, endAt: number) {
-  const prev = gameLogic.createBoard()
-  prev[startAt] = new gameLogic.Square('residential')
-
-  const curr = move(prev)
-  expect(curr[endAt].zone).toBe('residential')
+  return board
 }
 
-function expectToNotMove(move: MoveFunction, index: number) {
-  expectMove(move, index, index)
-}
+describe('moveLeft', () => {
+  test('moves square left', () => {
+    const i = 1
+    const prev = createBoard([i, 'residential', 2])
+    const curr = game.moveLeft(prev)
+    expect(curr[i - 1]).toEqual(prev[i])
+    expect(curr[i]).not.toEqual(prev[i])
+  })
 
-test('createBoard creates empty board of expected size', () => {
-  expect(gameLogic.createBoard()).toHaveLength(gameLogic.numSquares)
-  gameLogic.createBoard().forEach(square => expect(square.zone).toBeUndefined())
+  test('does not move square left when at left edge', () => {
+    const i = 0
+    const prev = createBoard([i, 'residential', 2])
+    const curr = game.moveLeft(prev)
+    expect(curr[i]).toEqual(prev[i])
+  })
+
+  test('merges squares of same zone when at left edge', () => {
+    const i = 0
+    const prev = createBoard([i, 'residential', 2], [i + 1, 'residential', 2])
+    const curr = game.moveLeft(prev)
+    expect(curr[i]).toEqual({ zone: 'residential', value: 4 })
+  })
+
+  test('does not combine when squares are at left edge but have different zones', () => {
+    const i = 1
+    const prev = createBoard([i, 'residential', 2], [0, 'commercial', 2])
+    const curr = game.moveLeft(prev)
+    expect(curr[i]).toEqual(prev[i])
+    expect(curr[0]).toEqual(prev[0])
+  })
 })
 
-test('moveLeft moves square left when space is free', () => {
-  expectMove(gameLogic.moveLeft, 2, 1)
-  expectToNotMove(gameLogic.moveLeft, 0)
+describe('moveRight', () => {
+  const rightCornerIndex = game.numCols - 1
+
+  test('moves square right', () => {
+    const i = 0
+    const prev = createBoard([i, 'residential', 2])
+    const curr = game.moveRight(prev)
+    expect(curr[i + 1]).toEqual(prev[i])
+    expect(curr[i]).not.toEqual(prev[i])
+  })
+
+  test('does not move square right when at right edge', () => {
+    const i = rightCornerIndex
+    const prev = createBoard([i, 'residential', 2])
+    const curr = game.moveRight(prev)
+    expect(curr[i]).toEqual(prev[i])
+  })
+
+  test('merges squares of same zone when at right edge', () => {
+    const i = rightCornerIndex
+    const prev = createBoard([i, 'residential', 2], [i - 1, 'residential', 2])
+    const curr = game.moveRight(prev)
+    expect(curr[i]).toEqual({ zone: 'residential', value: 4 })
+  })
+
+  test('does not combine when squares are at right edge but have different zones', () => {
+    const i = rightCornerIndex
+    const prev = createBoard([i - 1, 'residential', 2], [i, 'commercial', 2])
+    const curr = game.moveRight(prev)
+    expect(curr[i]).toEqual(prev[i])
+    expect(curr[i - 1]).toEqual(prev[i - 1])
+  })
 })
 
-test('moveUp moves square up when space is free', () => {
-  const start = (numCols * numRows) - 1
-  const end = start - numCols
-  expectMove(gameLogic.moveUp, start, end)
-  expectToNotMove(gameLogic.moveUp, 0)
+describe('moveUp', () => {
+  test('moves square up', () => {
+    const i = game.numCols
+    const prev = createBoard([i, 'residential', 2])
+    const curr = game.moveUp(prev)
+    expect(curr[0]).toEqual(prev[i])
+    expect(curr[i]).not.toEqual(prev[i])
+  })
+
+  test('does not move square up when at top edge', () => {
+    const i = 0
+    const prev = createBoard([i, 'residential', 2])
+    const curr = game.moveUp(prev)
+    expect(curr[i]).toEqual(prev[i])
+  })
+
+  test('merges squares of same zone when at top edge', () => {
+    const i = 0
+    const prev = createBoard([i, 'residential', 2], [game.numCols, 'residential', 2])
+    const curr = game.moveUp(prev)
+    expect(curr[i]).toEqual({ zone: 'residential', value: 4 })
+  })
+
+  test('does not combine when squares are at top edge but have different zones', () => {
+    const i = 0
+    const prev = createBoard([i, 'residential', 2], [game.numCols, 'commercial', 2])
+    const curr = game.moveUp(prev)
+    expect(curr[i]).toEqual(prev[i])
+    expect(curr[game.numCols]).toEqual(prev[game.numCols])
+  })
 })
 
-test('moveRight moves square right when space is free', () => {
-  expectMove(gameLogic.moveRight, 2, 3)
-  expectToNotMove(gameLogic.moveRight, numCols - 1)
-})
+describe('moveDown', () => {
+  test('moves square down', () => {
+    const i = 0
+    const prev = createBoard([i, 'residential', 2])
+    const curr = game.moveDown(prev)
+    expect(curr[game.numCols]).toEqual(prev[i])
+    expect(curr[i]).not.toEqual(prev[i])
+  })
 
-test('moveDown moves square down when space is free', () => {
-  expectMove(gameLogic.moveDown, 0, numCols)
+  test('does not move square down when at bottom edge', () => {
+    const i = game.numSquares - 1
+    const prev = createBoard([i, 'residential', 2])
+    const curr = game.moveDown(prev)
+    expect(curr[i]).toEqual(prev[i])
+  })
 
-  const end = (numCols * numRows) - 1
-  expectToNotMove(gameLogic.moveDown, end)
+  test('merges squares of same zone when at bottom edge', () => {
+    const i = game.numSquares - 1
+    const prev = createBoard([i, 'residential', 2], [i - game.numCols, 'residential', 2])
+    const curr = game.moveDown(prev)
+    expect(curr[i]).toEqual({ zone: 'residential', value: 4 })
+  })
+
+  test('does not combine when squares are at bottom edge but have different zones', () => {
+    const i = game.numSquares - 1
+    const prev = createBoard([i, 'residential', 2], [i - game.numCols, 'commercial', 2])
+    const curr = game.moveDown(prev)
+    expect(curr[i]).toEqual(prev[i])
+    expect(curr[game.numCols]).toEqual(prev[game.numCols])
+  })
 })
